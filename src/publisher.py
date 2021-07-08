ef44b65745c9a705700cbf927c9ec75f12f7a421
Adam Ruminski <cssrumi@gmail.com>
2021-07-08 13:33:21 +0200

    publisher changed to KafkaPublisher

--- src/publisher.py
+++ src/publisher.py
@@ -4,21 +4,21 @@ from config import CONFIG
 from event import RadonMeasurementEvent
 
 
-# class _KafkaPublisher:
-#     kafka_producer = KafkaProducer(
-#         bootstrap_servers=CONFIG.kafka_server,
-#         value_serializer=str.encode,
-#         key_serializer=str.encode
-#     )
-#
-#     @staticmethod
-#     def publish(event: RadonMeasurementEvent):
-#         _KafkaPublisher.kafka_producer.send(CONFIG.topic, key=event.key(), value=event.serialize())
+class _KafkaPublisher:
+    kafka_producer = KafkaProducer(
+        bootstrap_servers=CONFIG.kafka_server,
+        value_serializer=str.encode,
+        key_serializer=str.encode
+    )
+
+    @staticmethod
+    def publish(event: RadonMeasurementEvent):
+        _KafkaPublisher.kafka_producer.send(CONFIG.topic, key=event.key(), value=event.serialize())
 
 
 def _printer_publisher(event: RadonMeasurementEvent):
     print(f"Key: {event.key()}, event: {event.serialize()}")
 
 
-# publish = _KafkaPublisher.publish
-publish = _printer_publisher
+publish = _KafkaPublisher.publish
+# publish = _printer_publisher
