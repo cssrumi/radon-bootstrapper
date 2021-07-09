2384f7efeb766e4ab7e91c5ba708728f3f78dce9
Adam Ruminski <cssrumi@gmail.com>
2021-07-09 10:58:43 +0200

    Publisher reworkend

--- src/publisher.py
+++ src/publisher.py
@@ -4,21 +4,29 @@ from config import CONFIG
 from event import RadonMeasurementEvent
 
 
-class _KafkaPublisher:
+class _Publisher:
+    def publish(self, event: RadonMeasurementEvent):
+        raise NotImplementedError()
+
+
+class _PrinterPublisher(_Publisher):
+    def publish(self, event: RadonMeasurementEvent):
+        print(f"Key: {event.key()}, event: {event.serialize()}")
+
+
+class _KafkaPublisher(_Publisher):
     kafka_producer = KafkaProducer(
         bootstrap_servers=CONFIG.kafka_server,
         value_serializer=str.encode,
         key_serializer=str.encode
     )
 
-    @staticmethod
-    def publish(event: RadonMeasurementEvent):
+    def publish(self, event: RadonMeasurementEvent):
         _KafkaPublisher.kafka_producer.send(CONFIG.topic, key=event.key(), value=event.serialize())
 
 
-def _printer_publisher(event: RadonMeasurementEvent):
-    print(f"Key: {event.key()}, event: {event.serialize()}")
+_publishers = [_PrinterPublisher(), _KafkaPublisher()]
 
 
-publish = _KafkaPublisher.publish
-# publish = _printer_publisher
+def publish(event: RadonMeasurementEvent):
+    [publisher.publish(event) for publisher in _publishers]
